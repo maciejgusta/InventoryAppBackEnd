e216712a7de26c3906edea9a5932fe233ffb3a0f
MRST4SIO <mrstasio1@gmail.com>
2024-11-11 15:36:25 +0100

    automated commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "localhost",
+    host: "3.120.22.79",
     user: "admin",
     password: "admin",
     database: 'inventa'
